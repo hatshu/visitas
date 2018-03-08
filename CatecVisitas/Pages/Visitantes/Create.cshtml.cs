6ed3cb3075979ee0579c430979eea1a56e0ea729
bmartin <belenmartinsanchez@gmail.com>
2018-03-08 10:35:49 +0100

    First commit

---
+++ CatecVisitas/Pages/Visitantes/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CatecVisitas.Models;
+
+namespace CatecVisitas.Pages.Visitantes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly CatecVisitas.Models.PersonContext _context;
+
+        public CreateModel(CatecVisitas.Models.PersonContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Person Person { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Person.Add(Person);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
