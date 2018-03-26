f0a236789e9ad12a89f6145c961fe699303c3205
bmartin <bmartin@localhost>
2018-03-26 10:38:42 +0200

    Se gestiona para dar de alta primero visita y despues a?adir participantes a la misma

--- CatecVisitas/Pages/EnlaceVisitasPersonas/Create.cshtml.cs
+++ CatecVisitas/Pages/EnlaceVisitasPersonas/Create.cshtml.cs
@@ -11,11 +11,15 @@ namespace CatecVisitas.Pages.EnlaceVisitasPersonas
 {
     public class CreateModel : PageModel
     {
+        public string QueryStringEnlace;
+       
+
         private readonly CatecVisitas.Models.PersonContext _context;
 
         public CreateModel(CatecVisitas.Models.PersonContext context)
         {
             _context = context;
+           
         }
 
         public IActionResult OnGet()
@@ -25,15 +29,27 @@ namespace CatecVisitas.Pages.EnlaceVisitasPersonas
 
         [BindProperty]
         public EnlaceVisitaPersona EnlaceVisitaPersona { get; set; }
+        public IList<EnlaceVisitaPersona> EnlaceVisitaPersonaList { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
+           
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
+            QueryStringEnlace = Request.HttpContext.Request.QueryString.ToString();
+            string[] arrayQuery = QueryStringEnlace.Split('=','&');
+
+            //TODO: TRATAMIENTO PARA LA PRIMERA PARTE DEL QUERYSTRING
+
+
+            EnlaceVisitaPersona.PersonaID = Convert.ToInt32(arrayQuery[1]);
+            EnlaceVisitaPersona.VisitaID = Convert.ToInt32(arrayQuery[3]);
+
             _context.EnlaceVisitaPersona.Add(EnlaceVisitaPersona);
+            
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
