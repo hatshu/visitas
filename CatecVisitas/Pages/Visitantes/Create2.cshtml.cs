1b160d9ae0edda9dc9f868a8dfa7a4ff42bc89a8
bmartin <bmartin@localhost>
2018-03-29 00:50:07 +0200

    A?adimos Visitas con Personas Create2 para crear visitantes sin pasar visitas

---
+++ CatecVisitas/Pages/Visitantes/Create2.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatecVisitas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CatecVisitas.Pages.Visitantes
+{
+    public class Create2Model : PageModel
+    {
+        private readonly CatecVisitas.Models.PersonContext _context;
+
+        public Create2Model(CatecVisitas.Models.PersonContext context)
+        {
+            _context = context;
+
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Person Person { get; set; }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Person.FechaAlta = DateTime.Today.Date;       
+            _context.Person.Add(Person);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index2");
+
+        }
+
+        private IActionResult RedirectToPage(object pageName, object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CatecVisitas/Pages/Visitantes/VisitasConPersonas.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatecVisitas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatecVisitas.Pages.Visitantes
+{
+    public class VisitasConPersonasModel : PageModel
+    {
+        private readonly CatecVisitas.Models.PersonContext _context;
+
+        public VisitasConPersonasModel(CatecVisitas.Models.PersonContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Visita> Visita { get; set; }
+
+        public IList<Person> Persona { get; set; }
+
+        public IList<EnlaceVisitaPersona> EnlaceVisitasPersonas { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            Visita = await _context.Visita.ToListAsync();
+            Persona = await _context.Person.ToListAsync();
+            EnlaceVisitasPersonas = await _context.EnlaceVisitaPersona.ToListAsync();
+        }
+    }
+}
