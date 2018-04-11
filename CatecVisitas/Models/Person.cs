c16d60c2c6cc602da1d113a3576f5531446dd251
bmartin <belenmartinsanchez@gmail.com>
2018-04-11 13:10:39 +0200

    Validacion + mensajes personalizados de validacion

--- CatecVisitas/Models/Person.cs
+++ CatecVisitas/Models/Person.cs
@@ -9,16 +9,16 @@ namespace CatecVisitas.Models
     public class Person
     {
         public int Id { get; set; }
-        [MaxLength(50)]
+        [StringLength(50, ErrorMessage = "El campo nombre no puede tener mas de 50 caracteres")]
         [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
-        [MaxLength(50)]
+        [StringLength(50, ErrorMessage = "El campo apellido no puede tener mas de 50 caracteres")]
         [Required(ErrorMessage = "El apellido es obligatorio")]
         public string Apellidos { get; set; }
-        [MaxLength(50)]
+        [StringLength(50, ErrorMessage = "El campo DNI no puede tener mas de 50 caracteres")]
         [Required(ErrorMessage = "El DNI es obligatorio")]
         public string DNI { get; set; }
-        [MaxLength(75)]
+        [StringLength(75, ErrorMessage = "El campo empresa no puede tener mas de 50 caracteres")]
         public string Empresa { get; set; }
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime FechaAlta { get; set; }
