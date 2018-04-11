b36eddcca999f4659a87e60b06d8babdffeee392
bmartin <bmartin@localhost>
2018-04-11 12:45:21 +0200

    Configuraci?n de validacion de alta de un visitante

--- CatecVisitas/Models/Person.cs
+++ CatecVisitas/Models/Person.cs
@@ -8,12 +8,19 @@ namespace CatecVisitas.Models
 {
     public class Person
     {
-      public int Id { get; set; }
-      public string Nombre { get; set; }
-      public string Apellidos { get; set; }
-      public string DNI { get; set; }
-      public string Empresa { get; set; }
-      [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
-      public DateTime FechaAlta { get; set; }
-   }
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        public string Apellidos { get; set; }
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El DNI es obligatorio")]
+        public string DNI { get; set; }
+        [MaxLength(75)]
+        public string Empresa { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime FechaAlta { get; set; }
+    }
 }
