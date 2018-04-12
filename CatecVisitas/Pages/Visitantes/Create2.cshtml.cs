7d3b6dd736a849a4b2950677a03c5e4d2d9f6397
bmartin <bmartin@localhost>
2018-04-12 10:42:44 +0200

    Se valida tambien el campo dni si es duplicado para create2 de visitantes

--- CatecVisitas/Pages/Visitantes/Create2.cshtml.cs
+++ CatecVisitas/Pages/Visitantes/Create2.cshtml.cs
@@ -10,12 +10,17 @@ namespace CatecVisitas.Pages.Visitantes
 {
     public class Create2Model : PageModel
     {
+
+        public bool validationError = false;
+
+        public IList<Person> PersonList { get; set; }
+
         private readonly CatecVisitas.Models.PersonContext _context;
 
         public Create2Model(CatecVisitas.Models.PersonContext context)
         {
             _context = context;
-
+            PersonList = _context.Person.ToList();
         }
 
         public IActionResult OnGet()
@@ -29,6 +34,21 @@ namespace CatecVisitas.Pages.Visitantes
 
         public async Task<IActionResult> OnPostAsync()
         {
+            IQueryable<Person> PersonIQ = from s in _context.Person
+                                          select s;
+
+            var DNIID = Person.DNI;
+            if (!String.IsNullOrEmpty(DNIID))
+            {
+                PersonIQ = PersonIQ.Where(s => s.DNI.Equals(DNIID));
+                if (PersonIQ.Count() > 0)
+                {
+                    ModelState.AddModelError("DNIerror", "EL DNI esta duplicado");
+                    validationError = true;
+                }
+
+
+            }
             if (!ModelState.IsValid)
             {
                 return Page();
