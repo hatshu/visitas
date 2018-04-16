a51668f2a073b468c008d5da03b57155e2496673
bmartin <bmartin@localhost>
2018-04-16 13:39:57 +0200

    cambios menores visuales y pruebas con base de datos de ctc-sv126 sql 2008 Intentamos buscar alternativa para incompatibilidad entre skip y take que es valido para sql 2012 pero no para 2008 sin exito.

--- CatecVisitas/Models/PaginatedList.cs
+++ CatecVisitas/Models/PaginatedList.cs
@@ -15,7 +15,6 @@ namespace CatecVisitas.Models
         {
             PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-
             this.AddRange(items);
         }
 
@@ -38,10 +37,14 @@ namespace CatecVisitas.Models
         public static async Task<PaginatedList<T>> CreateAsync(
             IQueryable<T> source, int pageIndex, int pageSize)
         {
+
+            
+
             var count = await source.CountAsync();
-            var items = await source.Skip(
-                (pageIndex - 1) * pageSize)
+            var items = await source
+                .Skip((pageIndex - 1) * pageSize)
                 .Take(pageSize).ToListAsync();
+
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
 
