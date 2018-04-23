49dbf8436534c2e5ad13d96926968c6c616ef91c
bmartin <bmartin@localhost>
2018-04-23 09:07:07 +0200

    Cambios en Index para el buscador de fechas (transformar cualquier fecha en formato sql correcto)

--- CatecVisitas/Pages/Visitas/Index.cshtml.cs
+++ CatecVisitas/Pages/Visitas/Index.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CatecVisitas.Models;
 using System.Data.SqlClient;
-
+using System.Globalization;
 
 namespace CatecVisitas.Pages.Visitas
 {
@@ -58,17 +58,59 @@ namespace CatecVisitas.Pages.Visitas
 
                 var MinPageRank = (pageIndex - 1) * pageSize + 1;
                 var MaxPageRank = (pageIndex * pageSize);
-                visitaIQFecha = visitaIQFecha.Where(s => s.FechaVisita.ToShortDateString().Equals(searchString.Trim()) || s.Motivo.ToLower().Contains(searchString.ToLower()) || s.ResponsableCatec.ToLower().Equals(searchString.ToLower()));
-                //var visit = _context.Visita.FromSql($"SELECT * FROM (SELECT [RANK] = ROW_NUMBER() OVER (ORDER BY Hora DESC),* FROM Visita) A WHERE A.[RANK] BETWEEN {MinPageRank} AND {MaxPageRank}").ToList();
+                var searchCriteria = '%' + searchString + '%';
+                DateTime dateSearch;
+                var date = "";
+                bool onlyDigits = false , formatDate = false;
+                onlyDigits = searchString.All(c => c >= '0' && c <= '9' || c == '/' || c == '-');
+                //TODO: forzar a que la fecha sea correcta
+                formatDate = formatoCorrecto(searchString);
+                if (searchString.Contains("-") || searchString.Contains("/") &&  onlyDigits && searchString.Count() > 6 && searchString.Count() <= 10 && formatDate)
+                {
+                    //dateSearch = Convert.ToDateTime(searchString);
+                    dateSearch=Convert.ToDateTime(searchString);
+                    var dia = dateSearch.Day;
+                    var mes = dateSearch.Month;
+                    var anno = dateSearch.Year;
+                    if ( dia <= 9 && mes <= 9)
+                    {
+                        date = anno + "-0" + mes + "-0" + dia;
+                    }
+                    else if (dia <= 9)
+                    {
+                        date = anno + "-" + mes + "-0" + dia;
+                    }
+                    else if (mes <=9)
+                    {
+                        date = anno + "-0" + mes + "-" + dia;
+                    }
+                    else
+                    {
+                        date = anno + "-" + mes + "-" + dia;
+                    }
 
-                IQueryable<Visita> visitita = from s in visitaIQFecha.AsQueryable() select s;
 
-               
+                }
+                if (date.Equals(""))
+                {
+                    visitaIQFecha = visitaIQFecha.Where(s => s.FechaVisita.ToShortDateString().Equals(searchString.Trim()) || s.Motivo.ToLower().Contains(searchString.ToLower()) || s.ResponsableCatec.ToLower().Equals(searchString.ToLower()));
+                }
+               else
+                {
+                    var dateAux = Convert.ToDateTime(date);
+                    visitaIQFecha = visitaIQFecha.Where(s => s.FechaVisita.ToShortDateString().Equals(dateAux.ToShortDateString()));
+                }
+                sourceFull = visitaIQFecha.Count();
+                var visit = _context.Visita.FromSql($"SELECT * FROM (SELECT [RANK] = ROW_NUMBER() OVER (ORDER BY FechaVisita DESC, HORA DESC),* FROM Visita WHERE Motivo LIKE {searchCriteria} OR FechaVisita = {date} OR ResponsableCatec = {searchString} ) A WHERE A.[RANK] BETWEEN {MinPageRank} AND {MaxPageRank}").ToList();
+
+                IQueryable<Visita> visitita = from s in visit.AsQueryable() select s;
+
+                //sourceFull = visitita.Count();
                 
                 SearchString = searchString;
 
 
-                Visita = await PaginatedList<Visita>.CreateAsync(visitaIQFecha.AsNoTracking(), pageIndex ?? 1, pageSize, sourceFull);
+                Visita = await PaginatedList<Visita>.CreateAsync(visitita.AsNoTracking(), pageIndex ?? 1, pageSize, sourceFull);
             }
             else
             {
@@ -81,7 +123,7 @@ namespace CatecVisitas.Pages.Visitas
                 
                 var MinPageRank =  (pageIndex - 1) * pageSize + 1; 
                 var MaxPageRank = (pageIndex * pageSize);
-                var visit = _context.Visita.FromSql($"SELECT * FROM (SELECT [RANK] = ROW_NUMBER() OVER (ORDER BY Hora DESC),* FROM Visita) A WHERE A.[RANK] BETWEEN {MinPageRank} AND {MaxPageRank}").ToList();
+                var visit = _context.Visita.FromSql($"SELECT * FROM (SELECT [RANK] = ROW_NUMBER() OVER (ORDER BY FechaVisita DESC , Hora DESC),* FROM Visita) A WHERE A.[RANK] BETWEEN {MinPageRank} AND {MaxPageRank}").ToList();
 
                 IQueryable<Visita> visitita = from s in visit.AsQueryable() select s;
 
@@ -92,6 +134,30 @@ namespace CatecVisitas.Pages.Visitas
             }
            
 
+        }
+
+        private bool formatoCorrecto(string searchString)
+        {
+            if (searchString.Contains("-"))
+            {
+                var searchArray = searchString.Split('-');
+                if (searchArray.Count()==3 && searchArray[0].Length <= 2 && searchArray[1].Length <= 2 && searchArray[2].Length <= 4)
+                {
+                    return true;
+                }
+            }
+
+
+            if (searchString.Contains('/'))
+            {
+                var searchArray = searchString.Split('/');
+                if (searchArray.Count() == 3 && searchArray[0].Length <= 2 && searchArray[1].Length <= 2 && searchArray[2].Length <= 4)
+                {
+                    return true;
+                }
+
+            }
+            return false;
         }
     }
 }
